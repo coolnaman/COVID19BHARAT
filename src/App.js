5657e80483dd161a08066f614612e41a274e6c5d
awesomearpit <jarpit1298@gmail.com>
2020-04-12 01:38:32 +0530

    Add Data Table

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Box from "./Components/box";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Body from "./Components/Body";
 import axios from "axios";
+import CommonDataTable from "./Components/CommonDataTable";
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.appName= "COVID 19";
-    this.boxName="APP for Covid"
+    this.appName = "COVID 19";
+    this.boxName = "APP for Covid";
     console.log("constructor");
     this.state = {
-      caseTimeSeries : []
-    }
+      caseTimeSeries: [],
+    };
   }
 
-  async componentDidMount(){
-    try{
-        const {data} = await axios.get("https://api.covid19india.org/data.json");
-        var caseTimeArr = data ? data.cases_time_series :[]
-        console.log("data",data);
-    }catch(error){
-        console.log("error",error);
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        "https://api.covid19india.org/data.json"
+      );
+      var caseTimeArr = data ? data.cases_time_series : [];
+      console.log("data", data);
+    } catch (error) {
+      console.log("error", error);
     }
 
-    this.setState({caseTimeSeries:caseTimeArr.reverse()})
-}
+    this.setState({ caseTimeSeries: caseTimeArr.reverse() });
+  }
 
-  render(){
-    
-    return(
+  render() {
+    return (
       <>
-      <Header />
-      <Body caseData = {this.state.caseTimeSeries} />
-      <Footer />
-      
+        <Header />
+        {/* <Body caseData={this.state.caseTimeSeries} /> */}
+        <CommonDataTable commonData={this.state.caseTimeSeries} />
+        <Footer />
       </>
-    )
+    );
   }
 }
 
 export default App;
-
---
+++ src/Components/CommonDataTable.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import DataTable from "react-data-table-component";
+import axios from "axios";
+
+class CommonDataTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [
+        {
+          name: "Date",
+          sortable: true,
+          selector: "date",
+        },
+        {
+          name: "Daily Confirmed Cases",
+          sortable: true,
+          selector: "dailyconfirmed",
+        },
+        {
+          name: "Daily Deceased.",
+          sortable: true,
+          selector: "dailydeceased",
+        },
+        {
+          name: "daily Recovered",
+          sortable: true,
+          selector: "dailyrecovered",
+        },
+        {
+          name: "Total Deceased",
+          sortable: true,
+          selector: "totaldeceased",
+        },
+        {
+          name: "Total Recovered",
+          sortable: true,
+          selector: "totalrecovered",
+        },
+        {
+          name: "Total Confirmed",
+          sortable: true,
+          selector: "totalconfirmed",
+        },
+      ],
+      filterText: "",
+      resetPaginationToggle: false,
+      commonData: [],
+    };
+  }
+
+  onFilter = (e) => {
+    this.setState({ filterText: e.target.value });
+  };
+
+  onClear = () => {
+    this.setState({ filterText: "" });
+  };
+
+  render() {
+    const { columns, filterText } = this.state;
+    const { commonData } = this.props;
+    // const filteredItems = commonData.filter((item) =>
+    //   item.title.toLowerCase().includes(filterText.toLowerCase())
+    // );
+    return (
+      <>
+        <DataTable
+          // subHeader
+          // subHeaderComponent={
+          //   <FilterComponent
+          //     onFilter={this.onFilter}
+          //     onClear={this.onClear}
+          //     filterText={filterText}
+          //   />
+          // }
+          columns={columns}
+          data={commonData}
+          pagination={true}
+          defaultSortField
+        />
+      </>
+    );
+  }
+}
+
+const FilterComponent = (props) => {
+  return (
+    <div className="row filterComp">
+      <div className="filterComp-input">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Filter By Name"
+          value={props.filterText}
+          onChange={props.onFilter}
+        />
+      </div>
+
+      <button onClick={props.onClear}>X</button>
+    </div>
+  );
+};
+
+export default CommonDataTable;
